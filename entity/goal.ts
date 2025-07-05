2250ebde931de4d10fe685970c65030c96f752a9
zzrzhou <zzrzhou@tencent.com>
2025-07-05 10:31:40 +0800

    fix

--- entity/goal.ts
+++ entity/goal.ts
@@ -1,10 +1,23 @@
 import type { Exam } from './exam';
 
+export enum GoalStatus {
+  /* 准备中 */
+  preparing = 'preparing',
+  /* 进行中 */
+  doing = 'doing',
+  /* 已通过 */
+  passed = 'passed',
+  /* 挂起 */
+  suspended = 'suspended',
+}
+
 export type Goal = {
   id: string;
   name: string;
   /* subject: 学科 */
   subject: string;
+  /* status: 状态 */
+  status: GoalStatus;
   /* AI出题提示词 */
   ai_prompt: string;
   /* created_at: 创建时间 */
